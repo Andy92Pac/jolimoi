2bdd74751780d8723540abb6fd03be280839c2ae
andy92pac <andy.mpondo@gmail.com>
2020-01-12 20:05:32 +0100

    add front end check of number

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,23 @@
 submitForm = (event, form) => {
 	event.preventDefault()
 
+	let number = form.number.value
+
+	if (number === undefined) {
+		return alert("Nombre manquant")
+	}
+	if (isNaN(number)) {
+		return alert("L'argument n'est pas un nombre")
+	}
+	if (number <= 0 || number > 100) {
+		return alert("Le nombre doit être compris entre 0 et 100")
+	}
+
 	fetch('/romain', {
 		method: 'POST',
 		headers: {'content-type': 'application/json'},
 		body: JSON.stringify({
-			number: form.number.value
+			number
 		})
 	})
 	.then(res => res.json())
