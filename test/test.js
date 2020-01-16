c63ce9c49e0bd42d3285555edb248456b3d2d7b3
andy92pac <andy.mpondo@gmail.com>
2020-01-16 17:40:13 +0100

    update tests for sse

--- test/test.js
+++ test/test.js
@@ -1,59 +1,49 @@
+process.env.NODE_ENV = 'test';
+
 const expect = require('chai').expect;
 const request = require('request');
+const EventSource = require("eventsource");
 const app = require('../app')
 
 describe('Romanian', () => {
-	it('should fail because of missing number', () => {
-		const res = request.post({
-			url: 'http://localhost:3000/romanian', 
-		}, (err, res, body) => {
-			expect(res.statusCode).equal(400)
-			expect(JSON.parse(body).message).equal('Nombre manquant')
+	it('should fail because of missing number', (done) => {
+		request.get('http://localhost:3000/romanian'
+		, (err, res, body) => {
+			expect(res.statusCode).equal(404)
+			done()
 		})
 	})
 
-	it('should fail because of argument not being a number', () => {
-		const res = request.post({
-			url: 'http://localhost:3000/romanian', 
-			headers: {'content-type' : 'application/json'},
-			body: JSON.stringify({ number: 'hello' })
-		}, (err, res, body) => {
-			expect(res.statusCode).equal(401)
-			expect(JSON.parse(body).message).equal("L'argument n'est pas un nombre")
-		})
+	it('should fail because of argument not being a number', (done) => {
+		const source = new EventSource('http://localhost:3000/romanian/hello')
+		
+		source.addEventListener('message', function(e) {
+			let result = JSON.parse(e.data)
+			expect(result.message).equal("L'argument n'est pas un nombre")
+			source.close()
+			done()
+		}, false)
 	})
 
-	it('should fail because number is out of bounds', () => {
-		const res = request.post({
-			url: 'http://localhost:3000/romanian', 
-			headers: {'content-type' : 'application/json'},
-			body: JSON.stringify({ number: 200 })
-		}, (err, res, body) => {
-			expect(res.statusCode).equal(401)
-			expect(JSON.parse(body).message).equal("Le nombre doit être compris entre 0 et 100")
-		})
+	it('should fail because number is out of bounds', (done) => {
+		const source = new EventSource('http://localhost:3000/romanian/200')
+		
+		source.addEventListener('message', function(e) {
+			let result = JSON.parse(e.data)
+			expect(result.message).equal("Le nombre doit être compris entre 0 et 100")
+			source.close()
+			done()
+		}, false)
 	})
 
-	it('should fail because of number not being an integer', () => {
-		const res = request.post({
-			url: 'http://localhost:3000/romanian', 
-			headers: {'content-type' : 'application/json'},
-			body: JSON.stringify({ number: '3' })
-		}, (err, res, body) => {
-			expect(res.statusCode).equal(401)
-			expect(JSON.parse(body).message).equal('Le nombre doit être un entier')
-		})
+	it('should return correct converted number', (done) => {
+		const source = new EventSource('http://localhost:3000/romanian/3')
+		
+		source.addEventListener('result', function(e) {
+			let result = JSON.parse(e.data)
+			expect(result.convertedNumber).equal("III")
+			source.close()
+			done()
+		}, false)
 	})
-
-	it('should return correct converted number', () => {
-		const res = request.post({
-			url: 'http://localhost:3000/romanian', 
-			headers: {'content-type' : 'application/json'},
-			body: JSON.stringify({ number: 3 })
-		}, (err, res, body) => {
-			expect(res.statusCode).equal(200)
-			expect(JSON.parse(body).convertedNumber).equal('III')
-		})
-	})
-
 })
