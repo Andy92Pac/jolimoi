7956eac468ce5740115252634cd37f294abf5379
andy92pac <andy.mpondo@gmail.com>
2020-01-16 06:58:48 +0100

    update controller to use sse

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,34 @@
 const service = require('./../services')
 
-exports.romanian = (req, res, next) => {
+exports.romanian = async (req, res, next) => {
 	let number = req.body.number
 
 	if (number === undefined) {
-		return res.status(400).json({ message: "Nombre manquant"})
+		return res.status(400).end(JSON.stringify({ message: "Nombre manquant"}))
 	}
 	if (isNaN(number)) {
-		return res.status(401).json({ message: "L'argument n'est pas un nombre"})
+		return res.status(401).end(JSON.stringify({ message: "L'argument n'est pas un nombre"}))
 	}
 	if (number <= 0 || number > 100) {
-		return res.status(401).json({ message: "Le nombre doit être compris entre 0 et 100"})
+		return res.status(401).end(JSON.stringify({ message: "Le nombre doit être compris entre 0 et 100"}))
 	}
 	if (!Number.isInteger(number)) {
-		return res.status(401).json({ message: "Le nombre doit être un entier"})
+		return res.status(401).end(JSON.stringify({ message: "Le nombre doit être un entier"}))
 	}
+
+	res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	})
+
+	await sleep(10000)
 
 	let convertedNumber = service.convertToRomanianNumber(number)
 
-	res.json({ convertedNumber })
+	return res.end(JSON.stringify({ convertedNumber }))
+}
+
+sleep = (ms) => {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
