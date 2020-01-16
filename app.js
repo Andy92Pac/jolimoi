b9e43c865cef33ce0f8a10d0c30f14ce7d68e5f2
andy92pac <andy.mpondo@gmail.com>
2020-01-16 18:02:59 +0100

    fix missing logger

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
@@ -13,9 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 if (process.env.NODE_ENV !== 'test') {
-  app.use(express.logger());
+  app.use(logger('dev'));
 }
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
